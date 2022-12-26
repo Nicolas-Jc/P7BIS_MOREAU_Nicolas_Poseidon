4c9cf3728c46abcf52b04b9475baaa49569029d8
A151UJ <A151UJ@login.axa>
2022-12-26 22:00:39 +0100

    Feat : Mise en place rapports de Tests Jacoco et fichier de Logs

--- Poseiden-skeleton/src/main/java/com/openclassrooms/poseidon/repositories/RatingRepository.java
+++ Poseiden-skeleton/src/main/java/com/openclassrooms/poseidon/repositories/RatingRepository.java
@@ -3,7 +3,6 @@ package com.openclassrooms.poseidon.repositories;
 import com.openclassrooms.poseidon.models.RatingModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 
 public interface RatingRepository extends JpaRepository<RatingModel, Integer> {
 
